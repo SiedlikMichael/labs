b6d30e706d338bc2e79ab4e1354c6c6a91fbabf6
SiedlikMichael <michaelsiedlik@outlook.com>
2024-10-23 22:19:20 -0400

    Add files via upload

---
+++ BASIC LOOP ADD ON/BASIC LOOP ADD ON/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        const string correctCode = "13579"; //  // the correct password
+        const int maxAttempts = 5; // max attempts
+        int attempts = 0; // attempts
+        bool isLocked = true; // you walk up to the door...its locked
+
+        do
+        {
+            Console.Write("enter password");
+            string userInput = Console.ReadLine(); // Read user input
+            attempts++; //attempt counter
+
+            if (userInput == correctCode)
+            {
+                isLocked = false; // unlock the door
+                Console.WriteLine("BINGO!!!");
+            }
+            else if (attempts < maxAttempts)
+            {
+                Console.WriteLine("WRONG!!! try again.");
+            }
+        } while (isLocked && attempts < maxAttempts);
+
+        if (isLocked)
+        {
+            Console.WriteLine("TOO MANY FAILED ATTEMPS! Access denied!");
+        }
+    }
+}
---
+++ Basic Loops 10 21 24/Basic Loops 10 21 24/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const string correctCode = "13579"; // the correct password
+        const int maxAttempts = 5; // max attempts
+        int attempts = 0; 
+        bool isLocked = true; // you walk up to the door, its locked..
+
+        while (isLocked && attempts < maxAttempts)
+        {
+            Console.Write("Enter password");
+            string userInput = Console.ReadLine(); // Read user input
+            attempts++; //attempt counter..duh
+
+            if (userInput == correctCode)
+            {
+                isLocked = false; // Unlock the door
+                Console.WriteLine("BINGO!!!! The door is now unlocked.");
+            }
+            else if (attempts < maxAttempts)
+            {
+                Console.WriteLine("WRONG!!!!!try again.");
+            }
+        }
+
+        if (isLocked)
+        {
+            Console.WriteLine("TO MANY ATTEMPTS!  Access denied!");
+        }
+    }
+}
