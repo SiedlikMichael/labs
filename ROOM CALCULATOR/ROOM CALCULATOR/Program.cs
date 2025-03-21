e7f8a25f162feacee91e69263c2048930a3875a4
SiedlikMichael <michaelsiedlik@outlook.com>
2025-03-21 16:38:16 -0400

    Add files via upload

---
+++ ROOM CALCULATOR/ROOM CALCULATOR/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("enter length of room in feet");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.WriteLine("enter the width of the room in feet");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("enter height of the room in feet");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        double area = length * width;
+        double perimeter = 2 * (length + width);
+        string classification;
+        
+
+        if (area <= 250)
+        {
+            classification = "small";
+        }
+        else if (area < 650)
+        {
+            classification = "medium";
+        }
+        else
+        {
+            classification = "large";
+        }
+        Console.WriteLine($"Area of the room: {area:F2} square feet");
+        Console.WriteLine($"Perimeter of the room: {perimeter:F2} feet");
+        Console.WriteLine($"This is a {classification} room.");
+
+        double volume = area * height;
+        double surfaceArea = 2 * (length * width + length * height + width * height);
+
+        Console.WriteLine($"Volume of the room: {volume:F2} cubic feet");
+        Console.WriteLine($"Surface area of the room: {surfaceArea:F2} square feet");
+
+    }
+
+}
